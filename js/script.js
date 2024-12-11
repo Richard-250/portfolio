ed22a6fc7a4abf586d0ccee2a17fc7e24df5e45c
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-12-11 03:42:25 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let typed = new Typed('.typing', {
+    strings:['','Web Designer','','web Developer','','Graphic Designer','','Software Engineer','','Mobile App Developer','','IT Constructor','','Auto-mobile Programmer'],
+    typespeed: 100,
+    backspeed:60,
+    loop:true
+})
