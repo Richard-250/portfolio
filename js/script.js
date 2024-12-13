0f627adf24bca557b77c431b26a64d25a0a6eecf
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-12-13 07:59:20 -0800

    correct directory

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let typed = new Typed('.typing', {
-    strings:['','Web Designer','','web Developer','','Graphic Designer','','Software Engineer','','Mobile App Developer','','IT Constructor','','Auto-mobile Programmer'],
-    typespeed: 100,
-    backspeed:60,
-    loop:true
-})
+    strings: [
+        '', 'Web Designer', 
+        '', 'Web Developer', 
+        '', 'Graphic Designer', 
+        '', 'Software Engineer', 
+        '', 'Mobile App Developer', 
+        '', 'IT Constructor', 
+        '', 'Automobile Programmer'
+    ],
+    typeSpeed: 100, 
+    backSpeed: 60, 
+    backDelay: 500, 
+    loop: true
+});
